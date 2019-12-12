afb2afc49a2f5666bf9e04172ea26abae65600a9
minixd <52654295+minixd@users.noreply.github.com>
2019-12-12 18:41:22 +0100

    First commit - login/signup/logout and firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBuo9D3QnAkGUE5olqewuGsUyh05F2TJio",
+    authDomain: "apptest-b5352.firebaseapp.com",
+    databaseURL: "https://apptest-b5352.firebaseio.com",
+    projectId: "apptest-b5352",
+    storageBucket: "apptest-b5352.appspot.com",
+    messagingSenderId: "546922555640",
+    appId: "1:546922555640:web:1017623f373b772c1ab30d",
+    measurementId: "G-TQP4TH2101"
+  };
